53758dde974fd2e456a241ec459611359bc7107a
sheaba110 <abdallaayman140@gmail.com>
2026-02-09 01:21:11 +0200

    added a liberary for the dynamic webpages

--- offer_crawler/offer_crawler/settings.py
+++ offer_crawler/offer_crawler/settings.py
@@ -7,6 +7,14 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import sys
+
+# Windows-specific fix for Python 3.8+ and Twisted/Asyncio
+if sys.platform == "win32":
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
 BOT_NAME = "offer_crawler"
 
 SPIDER_MODULES = ["offer_crawler.spiders"]
@@ -15,16 +23,16 @@ NEWSPIDER_MODULE = "offer_crawler.spiders"
 ADDONS = {}
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+# Crawl responsibly by identifying yourself (and your website) on the user-agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+USER_AGENT = False
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = False
 
 # Concurrency and throttling settings
-CONCURRENT_REQUESTS = 16
-CONCURRENT_REQUESTS_PER_DOMAIN = 1
-DOWNLOAD_DELAY = 1.5
+# CONCURRENT_REQUESTS = 16
+# CONCURRENT_REQUESTS_PER_DOMAIN = 1
+# DOWNLOAD_DELAY = 1.5
 
 # Disable cookies (enabled by default)
 # COOKIES_ENABLED = False
@@ -34,7 +42,7 @@ DOWNLOAD_DELAY = 1.5
 
 # Override the default request headers:
 DEFAULT_REQUEST_HEADERS = {
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8,index.php",
     "Accept-Language": "en-US,en;q=0.9,ar;q=0.8",
     "Referer": "https://www.google.com/",
 }
@@ -62,6 +70,7 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
 # ITEM_PIPELINES = {
 #     "offer_crawler.pipelines.OfferCrawlerPipeline": 300,
 #     # "offer_crawler.pipelines.SaveToMongoPipeline": 800,
@@ -69,16 +78,16 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-AUTOTHROTTLE_ENABLED = True
+# AUTOTHROTTLE_ENABLED = True
 # The initial download delay
-AUTOTHROTTLE_START_DELAY = 5
+# AUTOTHROTTLE_START_DELAY = 5
 # The maximum download delay to be set in case of high latencies
-AUTOTHROTTLE_MAX_DELAY = 60
+# AUTOTHROTTLE_MAX_DELAY = 60
 # The average number of requests Scrapy should be sending in parallel to
 # each remote server
-AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
 # Enable showing throttling stats for every response received:
-AUTOTHROTTLE_DEBUG = False
+# AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
@@ -91,17 +100,24 @@ AUTOTHROTTLE_DEBUG = False
 # Set settings whose default value is deprecated to a future-proof value
 FEED_EXPORT_ENCODING = "utf-8"
 
-
-# DOWNLOAD_HANDLERS = {
-#     "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-#     "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
-# }
-
+# Scrapy-playwright setup section
 
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
-PLAYWRIGHT_BROWSER_TYPE = "firefox"
+
+
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
+
+
+
 PLAYWRIGHT_LAUNCH_OPTIONS = {
     "headless": False,
-    "timeout": 20 * 1000,  # 20 seconds
+    "timeout": 20 * 1000,
 }
-PLAYWRIGHT_CDP_URL = "http://localhost:9222"
+
+PLAYWRIGHT_BROWSER_TYPE = "chromium"
+
+DOWNLOAD_TIMEOUT = 60
+
--- offer_crawler/offer_crawler/spiders/badrgrb.py
+++ offer_crawler/offer_crawler/spiders/badrgrb.py
@@ -1,12 +1,24 @@
 import scrapy
+from scrapy_playwright.page import PageMethod
+
 
 class BadrgrbSpider(scrapy.Spider):
     name = "badrgrb"
     allowed_domains = ["elbadrgroupeg.store"]
-    
+
     def start_request(self):
         url = "https://elbadrgroupeg.store/index.php?route=product/catalog"
-        yield scrapy.Request(url, meta={"playwright": True})
-    
+        yield scrapy.Request(
+            url,
+            meta={
+                "playwright": True,
+                "playwright_page_methods": [
+                    PageMethod("wait_for_selector", "div.main-products"),
+                ],
+                "playwright_context": "default",
+            },
+        )
+
     def parse(self, response):
+
         pass
