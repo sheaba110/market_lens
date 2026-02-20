003fd5cf5e5e29ada59454a7a38f85900266f886
sheaba110 <abdallaayman140@gmail.com>
2026-02-20 17:44:39 +0200

    Signed-off-by: sheaba110 <abdallaayman140@gmail.com>

--- offer_crawler/offer_crawler/settings.py
+++ offer_crawler/offer_crawler/settings.py
@@ -23,7 +23,7 @@ NEWSPIDER_MODULE = "offer_crawler.spiders"
 ADDONS = {}
 
 
-# Crawl responsibly by identifying yourself (and your website) on the user-agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
